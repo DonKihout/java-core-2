f210fb6bdaf367ac50821c683dfe1a67652589d6
DonKihout <Vadim4ik.2017@yandex.ru>
2022-05-20 12:46:30 +0500

    Lesson 4

--- chat-client/src/main/java/ru/gb/net_chat/Application.java
+++ chat-client/src/main/java/ru/gb/net_chat/Application.java
@@ -20,4 +20,5 @@ public class Application extends javafx.application.Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
 }
--- chat-client/src/main/java/ru/gb/net_chat/Launcher.java
+++ chat-client/src/main/java/ru/gb/net_chat/Launcher.java
@@ -1,6 +1,7 @@
 package ru.gb.net_chat;
 
 public class Launcher{
+
     public static void main(String[] args) {
         Application.run(args);
     }
