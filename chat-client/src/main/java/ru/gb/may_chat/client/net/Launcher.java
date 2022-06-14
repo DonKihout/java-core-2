c58c5e5a58a7f59ba0524b378c3797b26f375e55
DonKihout <Vadim4ik.2017@yandex.ru>
2022-06-14 21:26:19 +0500

    Lesson 7 HomeWork

---
+++ chat-client/src/main/java/ru/gb/may_chat/client/net/Launcher.java
@@ -0,0 +1,9 @@
+package ru.gb.may_chat.client.net;
+
+import ru.gb.may_chat.client.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.run(args);
+    }
+}
